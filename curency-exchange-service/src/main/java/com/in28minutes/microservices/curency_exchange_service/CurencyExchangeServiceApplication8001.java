1a6cdfbc7fd657992d0fef719ef7d59371b6c0ef
ayselBatcoveanu <154974628+ayselBatcoveanu@users.noreply.github.com>
2024-11-16 18:59:07 +0200

    first commit

---
+++ curency-exchange-service/src/main/java/com/in28minutes/microservices/curency_exchange_service/CurencyExchangeServiceApplication8001.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.curency_exchange_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurencyExchangeServiceApplication8001 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurencyExchangeServiceApplication8001.class, args);
+	}
+
+}
---
+++ curency-exchange-service/src/test/java/com/in28minutes/microservices/curency_exchange_service/CurencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.curency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
